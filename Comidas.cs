0d07f5da7d4b1f53cf6afb681250d7d2607d7987
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-11-22 10:12:13 -0300

    Create Comidas.cs

---
+++ Comidas.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Data.SqlClient;
+
+namespace TP_3
+{
+    public class Comidas
+    {
+
+        
+        public void saudacao()
+        {
+
+            Console.WriteLine("HELLO");
+
+        }
+
+
+    }
+}
